39a89b1bd9b650720feca8b4a36e93f5cba1342a
Victor Cerqueira <victor.legat.cerqueira@gmail.com>
2025-07-16 19:07:57 -0300

    add(auth): Adicionado login/registrar-se

--- store/config/routes.rb
+++ store/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root "products#index"
+  resource :session
+  resources :passwords, param: :token
   get "up" => "rails/health#show", as: :rails_health_check
 
   resources :products
